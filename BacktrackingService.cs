1abf3bd65193d231714b14ce0e54d124f98073ec
Shawn Chang <aa8542110@yahoo.com.tw>
2025-07-02 00:38:52 +0800

    feat: implement 8 queens solution

---
+++ BacktrackingService.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace eight_queens;
+
+public interface ISolver
+{
+    List<int[]> GetResults();
+}
+
+public class BacktrackingService : ISolver
+{
+    private List<int[]> _solutions = new();
+    private int[] _queens = new int[8];
+    
+    public List<int[]> GetResults()
+    {
+        _solutions = new List<int[]>();
+        _queens = new int[8];
+        Backtrack(0);
+        return _solutions;
+    }
+    
+    private void Backtrack(int row)
+    {
+        if (row == 8)
+        {
+            int[] solution = new int[8];
+            Array.Copy(_queens, solution, 8);
+            _solutions.Add(solution);
+            return;
+        }
+        
+        for (int col = 0; col < 8; col++)
+        {
+            if (IsSafe(row, col))
+            {
+                _queens[row] = col;
+                Backtrack(row + 1);
+            }
+        }
+    }
+    
+    private bool IsSafe(int row, int col)
+    {
+        for (int i = 0; i < row; i++)
+        {
+            if (_queens[i] == col)
+                return false;
+            
+            if (Math.Abs(_queens[i] - col) == Math.Abs(i - row))
+                return false;
+        }
+        return true;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace eight_queens;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main()
+    {
+        var backtrackingService = new BacktrackingService();
+        var results = backtrackingService.GetResults();
+        PrintResults(results);
+        
+        Console.WriteLine("\n");
+    }
+    
+    static void PrintResults(List<int[]> solutions)
+    {
+        for (int i = 0; i < solutions.Count; i++)
+        {
+            Console.WriteLine($"//Solution {i + 1}:");
+            PrintBoard(solutions[i]);
+            Console.WriteLine();
+        }
+    }
+    
+    static void PrintBoard(int[] queens)
+    {
+        for (int row = 0; row < 8; row++)
+        {
+            for (int col = 0; col < 8; col++)
+            {
+                if (queens[row] == col)
+                    Console.Write("Q");
+                else
+                    Console.Write(".");
+            }
+            Console.WriteLine();
+        }
+    }
+}
